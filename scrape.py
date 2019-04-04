62aa7c32b389cd5bbf5ffc705b4d74d980c60408
Yallow <tylerbouma1@gmail.com>
2019-04-04 16:07:23 +0000

    scraper done. saving to twitterData

---
+++ scrape.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import datetime
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+url = 'http://ethans_fake_twitter_site.surge.sh/'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+
+tweetArr = []
+for tweet in content.find_all('div', attrs={"class": "tweetcontainer"}):
+    tweetObject = {
+        "author": tweet.find('h2', attrs={"class": "author"}).text,
+        "date": tweet.find('h5', attrs={"class": "dateTime"}).text,
+        "tweet": tweet.find('p', attrs={"class": "content"}).text,
+        "likes": tweet.find('p', attrs={"class": "likes"}).text,
+        "shares": tweet.find('p', attrs={"class": "shares"}).text
+    }
+    tweetArr.append(tweetObject)
+
+with open('twitterData.json', 'w') as outfile:
+    json.dump(tweetArr, outfile, default=myconverter)
